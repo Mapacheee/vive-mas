f46766a0f0882c317287812601e2738683f32492
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2025-07-06 14:38:51 -0400

    fixed background color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script>
-import Navbar from "$lib/components/Navbar.svelte";
+    import Navbar from "$lib/components/Navbar.svelte";
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        document.body.classList.add('body-bg');
+    });
 </script>
 
 <svelte:head>
     <title>Vive+ | Las mejores actividades</title>
 </svelte:head>
+
 <main>
     <Navbar></Navbar>
 
@@ -12,7 +18,14 @@ import Navbar from "$lib/components/Navbar.svelte";
 </main>
 
 <style>
-    main {
+    :global(.body-bg) {
         background: linear-gradient(90deg, #59B390 100%, rgba(89, 179, 144, 0.44));
+        margin: 0;
+        padding: 0;
+        min-height: 100vh;
+    }
+
+    main {
+        min-height: 100vh;
     }
 </style>
