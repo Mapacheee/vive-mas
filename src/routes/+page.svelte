9835d5017158b3d84b9471bb9acfed4151435d1a
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2025-07-08 21:41:38 -0400

    home page working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,31 @@
             </div>
         </div>
     </div>
+
+    <div class="vive-container">
+        <h3>¿Qué es Vive+?</h3>
+        <div class="vive-container-text">
+            <p>Somos una organización sin fines de lucro dedicada a mejorar la calidad de vida de las personas<p/>
+            <p> a través de actividades recreativas y culturales. Nos enfocamos en ofrecer una amplia variedad</p>
+            <p>e actividades que fomentan la sociabilización y entretenimiento.</p>
+        </div>
+    </div>
+
+    <div class="talleres-container">
+        <div class="talleres-container-text">
+            <p>¡Desde talleres de arte</p>
+            <p>hasta clases de baile!</p>
+            <p>Para todos los gustos.</p>
+        </div>
+        <img src="home/home_image2.webp" alt="Imagen no encontrada">
+    </div>
+
+    <div class="actividades-container">
+        <h3>Actividades destacadas de la semana</h3>
+        <div class="actividades-container-carrusel">
+            <p>Carrusel en WIP</p>
+        </div>
+    </div>
 </main>
 
 <style>
@@ -72,5 +97,63 @@
 
     .cta-button:hover {
         transform: scale(1.05);
+    }
+
+    .vive-container {
+        font-weight: bold;
+        font-size: 1.5rem;
+
+        h3 {
+            margin-left: 3%;
+            color: #E47C5D;
+        }
+    }
+
+    .vive-container-text {
+        background: #D9D9D9;
+        color: #152B3C;
+        padding: 5px;
+
+        p {
+            text-align: center;
+            margin: 0;
+            line-height: 2rem;
+
+        }
+    }
+
+    .talleres-container {
+        display: flex;
+        margin-top: 3%;
+        img {
+            border-radius: 20px;
+            margin-left: 15%;
+            height: 300px;
+            width: auto;
+        }
+    }
+    .talleres-container-text {
+        border-radius: 10px;
+        padding: 8px;
+        margin-top: 3%;
+        margin-left: 20%;
+        margin-bottom: 5%;
+        color: #152B3C;
+        font-weight: bold;
+        font-size: 1.5rem;
+        background: #D9D9D9;
+    }
+
+    .actividades-container {
+        font-size: 1.5rem;
+        font-weight: bold;
+        h3 {
+            margin-left: 3%;
+            color: #E47C5D;
+        }
+    }
+
+    .actividades-container-carrusel {
+        margin-left: 40%;
     }
 </style>
