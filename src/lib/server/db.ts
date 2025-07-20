4ebacd116e21cf5a34f45cc60a521373ad418cd4
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2025-07-20 17:28:25 -0400

    migrate sqlite to vercel blob

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,13 +1,4 @@
-//import Database from 'better-sqlite3';
-import { dev } from '$app/environment';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from '@vercel/postgres';
 
-const dbFile = dev ? 'prisma/dev.db' : 'prisma/prod.db';
-
-//export const db = new Database(dbFile);
-//db.pragma('journal_mode = WAL'); this for use sqlite
-
 export const db = drizzle(sql);
-
-
--- src/routes/api/activities/request/+server.ts
+++ src/routes/api/activities/request/+server.ts
@@ -1,28 +1,26 @@
 import { json, error } from '@sveltejs/kit';
 import { db } from '$lib/server/db.js';
+import { sql } from '@vercel/postgres';
 
-import { dev } from '$app/environment';
-console.log('[SQLite] Opening', dev ? 'prisma/dev.db' : 'prisma/prod.db');
-
-// @ts-ignore
 export async function POST({ request }) {
     const body = await request.json();
-    const { title, location, imageUrl, description, schedule, address,
-        organizer, contact, website, maxParticipants } = body;
+    const { title, location, imageUrl, description, schedule,
+        address, organizer, contact, website, maxParticipants } = body;
 
-    if (!title || !location || !imageUrl || !description ||
-        !schedule || !address || !organizer || !contact || !website || maxParticipants <= 0)
-        throw error(400, 'faltan campos o inválidos');
+    if (!title || !location || !imageUrl || !description
+        || !schedule || !address || !organizer || !contact || !website || maxParticipants <= 0)
+        throw error(400, 'Faltan campos o inválidos');
 
     const scheduleDate = new Date(schedule);
     if (scheduleDate <= new Date()) throw error(400, 'La fecha debe ser futura');
 
-    const insert = db.prepare(`
-    INSERT INTO pending (title, location, imageUrl, description, schedule, address, 
-                         organizer, contact, website, maxParticipants)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    // @ts-ignore
+    await db.execute(sql`
+    INSERT INTO pending (title, location, imageUrl, description, 
+                         schedule, address, organizer, contact, website, maxParticipants)
+    VALUES (${title}, ${location}, ${imageUrl}, ${description}, 
+            ${schedule}, ${address}, ${organizer}, ${contact}, ${website}, ${maxParticipants})
   `);
-    insert.run(title, location, imageUrl, description, schedule, address, organizer, contact, website, maxParticipants);
 
     return json({ ok: true });
 }
