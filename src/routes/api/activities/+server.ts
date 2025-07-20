51e9e5a11faaae48b559be756e0fbadb9b666e4a
Benjamin Isasmendi <70007365+Mapacheee@users.noreply.github.com>
2025-07-20 18:40:06 -0400

    fixed a dump error xd

--- src/routes/api/activities/+server.ts
+++ src/routes/api/activities/+server.ts
@@ -1,7 +1,9 @@
 import { json } from '@sveltejs/kit';
-import { db } from '$lib/server/db';
+import { prisma } from '$lib/server/db.js';
 
 export async function GET() {
-    const rows = await db`SELECT * FROM activities ORDER BY createdAt DESC`;
+    const rows = await prisma.pending.findMany({
+        orderBy: { createdAt: 'desc' }
+    });
     return json(rows);
 }
--- src/routes/api/activities/[id]/+server.ts
+++ src/routes/api/activities/[id]/+server.ts
@@ -1,9 +1,11 @@
 import { json, error } from '@sveltejs/kit';
-import { db } from '$lib/server/db.js';
+import { prisma } from '$lib/server/db.js';
 
 export async function GET({ params }) {
     const id = Number(params.id);
-    const rows = await db`SELECT * FROM activities WHERE id = ${id}`;
+    const rows = await prisma.pending.findMany({
+        orderBy: { createdAt: 'desc' }
+    });
     // @ts-ignore
     return rows.rowCount === 0 ? error(404, 'Not found') : json(rows[0])
 }
